34cf0a8b3e730126a70587839ef8720d9ec55d26
Mason Banks <YokiStation@Masons-MacBook-Pro.local>
2019-01-08 14:54:25 +0000

    add styling and scaling changes to navbar

---
+++ themes/casper2-theme-master/assets/js/navcollapse.js
@@ -0,0 +1,5 @@
+// ************ Collapse navbar when section is clicked on (For mobile version only) ************
+$('.navbar-nav>li>a').on('click', function () {
+  $('.navbar-collapse').collapse('hide');
+});
+
---
+++ themes/casper2-theme-master/assets/js/scrolltotop.js
@@ -0,0 +1,17 @@
+// ************************************************************************
+// ************************* Scroll to Top button *************************
+// ************************************************************************
+$(window).scroll(function () {
+  if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+    $('#return-to-top').fadeIn(200);    // Fade in the arrow
+    // $('#MainNav').attr('style', 'opacity: 0.9;'); // slightly transparent navbar when scrolled down
+  } else {
+    $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+    $('#MainNav').attr('style', 'opacity: 1;');
+  }
+});
+$('#return-to-top').click(function () {      // When arrow is clicked
+  $('body,html').animate({
+    scrollTop: 0                       // Scroll to top of body
+  }, 500);
+});
